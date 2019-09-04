4c55f988df63c5d2d2e7d46f24776c20fcc1fd8d
Kimi no Na wa <hoanganh.theodore@icloud.com>
2019-09-04 15:45:34 -0400

    init

--- index.r
+++ index.r
@@ -7,7 +7,52 @@
 # This R program and its repository on Github is a course project. This project is built upon the MRCA (Most Recent Common Ancestors) paper by Dr. Chang (Yale University) et al. 
 # A copy of Dr. Chang's paper could also be found in this repository.
 
+### Exercise 1: Simulating Probabilities
 ########## Start of Program ###########
+# (a) Rolling a die
+#(i) 12-sided die, 500 times, pie chart 
+die1 <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+sample1 <- sample(die1, 500, replace=TRUE)
+sample1
+for (i in length(sample1)) {
+    if (sample1[i] == 1) {
+        number1 += 1
+    }
+    else if (sample1[i] == 2) {
+        number1 += 1
+    }
+    else if (sample1[i] == 3) {
+        number1 += 1
+    }
+    else if (sample1[i] == 4) {
+        number1 += 1
+    }
+    else if (sample1[i] == 5) {
+        number1 += 1
+    }
+    else if (sample1[i] == 6) {
+        number1 += 1
+    }
+    else if (sample1[i] == 7) {
+        number1 += 1
+    }
+    else if (sample1[i] == 8) {
+        number1 += 1
+    }
+    else if (sample1[i] == 9) {
+        number1 += 1
+    }
+    else if (sample1[i] == 10) {
+        number1 += 1
+    }
+    else if (sample1[i] == 11) {
+        number1 += 1
+    }
+    else if (sample1[i] == 12) {
+        number1 += 1
+    }
+}
+
 
 ########## End of Program ############
 
