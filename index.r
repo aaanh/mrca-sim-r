60d5ebe01bee12bcd51cd24d907ff35ad607a904
Kimi no Na wa <hoanganh.theodore@icloud.com>
2019-09-04 15:28:02 -0400

    init

---
+++ index.r
@@ -0,0 +1,15 @@
+# Nguyen Hoang Anh
+# John Abbott College, Montreal, CANADA.
+# Github: https://github.com/hirumaakane-ha/r-project-2019
+# License: MIT License
+# Course: Statistics
+# Instructor: Luiz T. Kazuo
+# This R program and its repository on Github is a course project. This project is built upon the MRCA (Most Recent Common Ancestors) paper by Dr. Chang (Yale University) et al. 
+# A copy of Dr. Chang's paper could also be found in this repository.
+
+########## Start of Program ###########
+
+########## End of Program ############
+
+# Clear screen
+cat('\014')
