e80208c81e43cf461c8b7d958817f42bde6df3fa
Kimi no Na wa <hoanganh.theodore@icloud.com>
2019-09-04 22:50:49 -0400

    solved 1ai

--- index.r
+++ index.r
@@ -7,53 +7,28 @@
 # This R program and its repository on Github is a course project. This project is built upon the MRCA (Most Recent Common Ancestors) paper by Dr. Chang (Yale University) et al. 
 # A copy of Dr. Chang's paper could also be found in this repository.
 
+# Prerequisites
+## Details can be found in README.md
+
+# Install & load pacman
+install.packages("pacman")
+require(pacman)
+# Use pacman to install and require other packages
+pacman::p_load(pacman, dplyr, GGally, ggplot2, ggthemes, 
+               ggvis, httr, lubridate, plotly, rio, rmarkdown, shiny, 
+               stringr, tidyr) 
+
 ### Exercise 1: Simulating Probabilities
 ########## Start of Program ###########
 # (a) Rolling a die
 #(i) 12-sided die, 500 times, pie chart 
-die1 <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
-sample1 <- sample(die1, 500, replace=TRUE)
-sample1
-for (i in length(sample1)) {
-    if (sample1[i] == 1) {
-        number1 += 1
-    }
-    else if (sample1[i] == 2) {
-        number1 += 1
-    }
-    else if (sample1[i] == 3) {
-        number1 += 1
-    }
-    else if (sample1[i] == 4) {
-        number1 += 1
-    }
-    else if (sample1[i] == 5) {
-        number1 += 1
-    }
-    else if (sample1[i] == 6) {
-        number1 += 1
-    }
-    else if (sample1[i] == 7) {
-        number1 += 1
-    }
-    else if (sample1[i] == 8) {
-        number1 += 1
-    }
-    else if (sample1[i] == 9) {
-        number1 += 1
-    }
-    else if (sample1[i] == 10) {
-        number1 += 1
-    }
-    else if (sample1[i] == 11) {
-        number1 += 1
-    }
-    else if (sample1[i] == 12) {
-        number1 += 1
-    }
-}
 
-
+die <- (1:12)
+dieRoll <- sample(die, 500, replace=TRUE)
+tb <- table(dieRoll)
+piepercent <- round(100*dieRoll/sum(dieRoll), 1)
+pie(tb, labels=(1:12), radius=1, col=rainbow(12))
+barplot(tb, xlab="Roll", ylab="Freq")
 ########## End of Program ############
 
 # Clear screen
