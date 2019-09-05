4a4a8e6117f7f4a12ae1ce1691802dd4523715f8
Nogamiok <hoanganh.theodore@icloud.com>
2019-09-05 16:20:39 -0400

    exercise 1aii, 1aiii

--- index.r
+++ index.r
@@ -19,7 +19,7 @@ pacman::p_load(pacman, dplyr, GGally, ggplot2, ggthemes,
                stringr, tidyr) 
 
 ### Exercise 1: Simulating Probabilities
-########## Start of Program ###########
+
 # (a) Rolling a die
 #(i) 12-sided die, 500 times, pie chart 
 
@@ -29,7 +29,34 @@ tb <- table(dieRoll)
 piepercent <- round(100*dieRoll/sum(dieRoll), 1)
 pie(tb, labels=(1:12), radius=1, col=rainbow(12))
 barplot(tb, xlab="Roll", ylab="Freq")
-########## End of Program ############
+
+# (ii) 12-sided die, 500 times, pie chart, 1 is x3 more likely
+
+die <- (1:12)
+probability <- c(0.3, rep(0.1, 11))
+dieRoll <- sample(die,500,replace=TRUE, prob=probability)
+tb <- table(dieRoll)
+pie(tb, labels=(1:12), radius=1, col=rainbow(12))          
+
+# (iii) 2x(12-sided dice), 10000 times, P(sum=10)=?
+
+die <- (1:12)
+fair_prob <- rep(0.1,12)
+dieRoll1 <- sample(die, 10000, replace=TRUE, prob=fair_prob)
+dieRoll2 <- sample(die, 10000, replace=TRUE, prob=fair_prob)
+sumn <- function(){
+  counter <- 0
+  for (i in 1:10000){
+    if ((dieRoll1[i] + dieRoll2[i]) == 10){
+      counter = counter + 1
+    }
+    i = i + 1
+  }
+  return(counter)
+}
+desiredoutcomes <- sumn()
+estprob <- c(desiredoutcomes/10000)
+estprob
 
 # Clear screen
 cat('\014')
