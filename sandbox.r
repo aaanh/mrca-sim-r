5a1e972bf6b3f96d91dbb8c6a838a9c29bdcac47
Nogamioka <hoanganh.theodore@icloud.com>
2019-09-13 18:44:54 -0400

    moar progress in sandbox

--- sandbox.r
+++ sandbox.r
@@ -1,7 +1,9 @@
 total_cups <- 33357600
 # let P[X=1]="probability fewer than 1300 cups were winning"
 # let 33357600C8000 be the way of choosing for cups to be sent to Tim Hortons
-# let P[X=0]="probability more than 1300 cups were winning"
+# let P[X=0]="probability more than 1300 cups were losing"
+
+as.numeric(combn(total_cups, 8000))
 
 type <- c('W', 'L')
 cups <- sample(type, total_cups, replace=TRUE, prob=c(1/6,5/6))
@@ -46,3 +48,26 @@ print("winning cups @ kirkland: ")
 print(c_win)
 print("losing cups @ kirkland: ")
 print(c_lose)
+
+##################################
+pbinom(8000, 1300, 1/6)
+x <- 0:8000
+plot(x, pbinom(x,1300,1/6), type='h')
+
+#################################
+# let k=1:1300
+# p[X=1] = 1/6
+# P[X=0] = 5/6
+
+prob_winning <- function() {
+  p_k <- 0
+  for (k in 1:1300) {
+    p_k <- p_k+length(combn(1300, k))*((1/6)^k)*((5/6)^(1300-k))
+    print(p_k)
+  }
+  return(p_k)
+}
+
+pk <- prob_winning()
+print(pk)
+
