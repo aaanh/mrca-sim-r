ae1c1fd1c75d6032d37a4a4b1885673b455e4c48
Nguyen Hoang Anh <nogami@Nguyens-MacBook-Pro.local>
2019-10-23 11:48:48 -0400

    stuff

--- sandbox2.r
+++ sandbox2.r
@@ -1,4 +1,4 @@
-n <- 5
+n <- 10
 
 # suppose there is an arbitrary gen(x) as  the current gen
 # the enumerator function is going to choose the gen(x-1)
@@ -11,9 +11,6 @@ init <- function(n) {
     return(g)         
 }
 
-g <- init(n)
-print(g)
-
 gen_x <- function(g, n) {
     for (i in 1:n) {
         g[[i]] <- union(g[[i]], sample(1:n, 2, replace=TRUE)) 
@@ -21,22 +18,27 @@ gen_x <- function(g, n) {
     return(g)
 }
 
+g <- init(n)
+
 g <- gen_x(g, n)
+
+checkMrca <- function(g, n) {
+    for (i in 1:n) {
+        if (length(g[[i]]) == n) {
+            return(TRUE)
+        }
+        else {
+            return(FALSE)
+        }
+    }
+}
 
 gencounter <- 0
 
-yesmrca <- FALSE
+while (checkMrca(g,n) != TRUE) {
+    gencounter <- gencounter + 1
+    g <- gen_x(g,n)
+    print(gencounter)
+}
 
-while (yesmrca <- FALSE) {
-    for (i in 1:n) {
-        if (length(g[[i]]) != n) {
-            g <- gen_x(g, n)
-            gencounter <- gencounter + 1
-        }
-        else {
-            yesmrca <- FALSE
-        }
-}
-}
 print(g)
-print(gencounter)
