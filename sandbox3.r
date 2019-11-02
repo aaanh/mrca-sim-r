1303387d0fff9dada38c571d7d28ebc826939e12
Hoang Anh <hoanganh.theodore@icloud.com>
2019-11-02 01:01:56 -0400

    new light

--- sandbox3.r
+++ sandbox3.r
@@ -1,1 +1,17 @@
-plot(exp(-1:0, 0.01))
+# MRCA simulation program
+# Iteration by Nguyen Hoang Anh, based on instructor L. K. Takei's abstraction
+# MIT License, 2019
+
+# Initialize
+n <- as.integer(readline(prompt="Input population size: "))
+l_d <- list()
+for (i in 1:n) {
+    gen0[[i]] <- i
+}
+l_d
+
+l_d
+# Functions
+
+# Main
+
