f3edcc71a752ee31376faf840feea18a282dc2b6
Hoang Anh <hoanganh.theodore@icloud.com>
2019-12-03 23:29:35 -0500

    commented on graph.r

--- part-3/graph.r
+++ part-3/graph.r
@@ -1,7 +1,12 @@
 require(plotly)
-setwd("~/dev/r-project-2019/results/")
+# setwd("~/dev/r-project-2019/results/") # For personal macOS dev env (MacBook)
+# setwd("C:/Users/Nogamioka/Desktop/dev/r-project-2019/results/") # For personal Windows dev env (Desktop)
+# setwd("~/dev/r-project-2019/results/") # For personal Windows dev env (Laptop)
+
+# !!! Only use for macOS graphical image exporting !!!
 if (!require("processx")) install.packages("processx")
 orca(p, "tmrca-results.png")
+#-----------------------------------
 
 df <- read.csv("results.csv")
 
@@ -21,18 +26,6 @@ p <- plot_ly(data = df, x = ~c(1:25)) %>%
     # add_trace(y = ~vector_4000, name = "n = 4000", marker = list(color = "rgb(219, 155, 50)", symbol = "square-dot"), mode="lines+markers") %>%
     # add_trace(y = ~vector_5000, name = "n = 5000", marker = list(color = "rgb(49, 214, 63)", symbol = "x"), mode="lines+markers") %>%
     # add_trace(y = ~vector_10000, name = "n = 10000", marker = list(color = "rgb(214, 51, 51)", symbol = "triangle-up"), mode="lines+markers") %>%
-
-fivenum_100 <- fivenum(df$vector_100)
-fivenum_1000 <- fivenum(df$vector_1000)
-fivenum_4000 <- fivenum(df$vector_4000)
-fivenum_5000 <- fivenum(df$vector_5000)
-fivenum_10000 <- fivenum(df$vector_10000)
-
-fivenum_100
-fivenum_1000
-fivenum_4000
-fivenum_5000
-fivenum_10000
 
 mean_100 <- mean(df$vector_100)
 mean_1000 <- mean(df$vector_1000)
